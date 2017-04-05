30ba7bff17d2b7577f2e48fef47ed0e07d5b2165
scannella <scannella@bishopireton.org>
2017-04-05 09:38:52 -0400

    Never mind, added a paddle with some code.  found some articles that also used  paddle-like objects and learned how to make one with that, still working on ball

---
+++ app/src/main/java/paddle.java
@@ -0,0 +1,72 @@
+/**
+ * Created by scannella on 4/5/2017.
+ */
+
+import android.graphics.RectF;
+
+public class Paddle {
+
+    // get a rectangle object
+    private RectF rect;
+
+    // paddle height and width
+    private float width;
+    private float height;
+
+    // left side of paddle
+    private float side;
+
+    // top of paddle
+    private float top;
+
+    //paddle speed
+    private float paddleSpeed;
+
+    //Ways paddle can move
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    //Direction the paddle is moving
+    private int paddleMoving = STOPPED;
+
+    //Constructor
+    public Paddle(int screenX, int screenY) {
+        width = 130;
+        height = 20;
+
+        //put paddle in the middle
+        side = screenX / 2;
+        top = screenY - 20;
+
+        rect = new RectF(side, top, side + width, top + height);
+
+        //Speed of paddle (pixels per sec.)
+        paddleSpeed = 350;
+    }
+
+    //getter
+    public RectF getRect() {
+        return rect;
+    }
+
+    //setter
+    public void setMovement(int direction) {
+        paddleMoving = direction;
+    }
+
+//update, will update when the paddle needs to change direction
+    public void update(long fps) {
+        if (paddleMoving == LEFT) {
+            side = side - paddleSpeed / fps;
+        }
+
+        if (paddleMoving == RIGHT) {
+            side = side + paddleSpeed / fps;
+        }
+
+        rect.left = side;
+        rect.right = side + width;
+    }
+
+}
